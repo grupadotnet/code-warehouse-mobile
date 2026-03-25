1a8ba20cfcb9299d14dde360b6bf891eae97d075
Bober1337IT <bartekpietras03@gmail.com>
2026-03-25 21:30:29 +0100

    initial commit

---
+++ app/src/main/java/pk/knpmi/barcode/MainActivity.kt
@@ -0,0 +1,22 @@
+package pk.knpmi.barcode
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Surface
+import pk.knpmi.barcode.ui.theme.ProjectBarcodeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProjectBarcodeTheme {
+                Surface{
+
+                }
+            }
+        }
+    }
+}
