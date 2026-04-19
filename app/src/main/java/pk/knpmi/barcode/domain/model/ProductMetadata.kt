631503a47179af7898382c23b611ca39830ec7d5
Bober1337IT <bartekpietras03@gmail.com>
2026-04-19 19:15:21 +0200

    add comment to ProductMetadata class

--- app/src/main/java/pk/knpmi/barcode/domain/model/ProductMetadata.kt
+++ app/src/main/java/pk/knpmi/barcode/domain/model/ProductMetadata.kt
@@ -1,5 +1,6 @@
 package pk.knpmi.barcode.domain.model
 
+// Will be used for displaying in drop down menu, for quick select
 data class ProductMetadata(
     val producers: List<String>,
     val categories: List<String>,
