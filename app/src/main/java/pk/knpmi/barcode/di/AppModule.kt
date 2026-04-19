f29ac5ba3a45777bcf59c52984e354ba83b69e4d
Bober1337IT <bartekpietras03@gmail.com>
2026-04-19 20:27:48 +0200

    provide fake LocalisationRepository and ProductRepository in AppModule

--- app/src/main/java/pk/knpmi/barcode/di/AppModule.kt
+++ app/src/main/java/pk/knpmi/barcode/di/AppModule.kt
@@ -1,11 +1,28 @@
 package pk.knpmi.barcode.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import pk.knpmi.barcode.data.repository.fake_repository.FakeLocalisationRepository
+import pk.knpmi.barcode.data.repository.fake_repository.FakeProductRepository
+import pk.knpmi.barcode.domain.repository.LocalisationRepository
+import pk.knpmi.barcode.domain.repository.ProductRepository
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Provides
+    @Singleton
+    fun provideLocalisationRepository(): LocalisationRepository{
+        return FakeLocalisationRepository()
+    }
+
+    @Provides
+    @Singleton
+    fun provideProductRepository(): ProductRepository{
+        return FakeProductRepository()
+    }
 }
